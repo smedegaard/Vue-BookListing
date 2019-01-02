730fccce2228473a923fc90b52f5428694cee257
Anders S. Pedersen <anders.s.pedersen@webstep.no>
2019-01-02 13:04:09 +0100

    module 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,18 +1,22 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <ul>
-      <book-item v-for="book in books" :book='book'></book-item>
-    </ul>
-  </div>
+<div>
+  <h1>{{ title }}</h1>
+  <ul>
+    <book-item v-for="book in books" :book='book'></book-item>
+    <br/><hr/>
+    <book-form @addBook='appendBook'></book-form>
+  </ul>
+</div>
 </template>
 
 <script>
-  import BookItem from './BookItem';
+import BookItem from './BookItem';
+import BookForm from './BookForm';
   export default {
       name: 'BookList',
       components: {
-          BookItem
+          BookItem,
+          BookForm
       },
       data() {
           return {
@@ -23,7 +27,12 @@
                   {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
                 ]
             }
-        }
+      },
+      methods: {
+          appendBook(bookTitle, bookAuthor) {
+              this.books.push({title: bookTitle, author: bookAuthor})
+          }
+      }
     }
 </script>
 
